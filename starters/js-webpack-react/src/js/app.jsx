176a96d33e2f3c7c7d4fb373ae1dda040ae31add
Tom Marien <tommarien@gmail.com>
2019-04-30 10:56:02 +0200

    Convert app to function

--- starters/js-webpack-react/src/js/app.jsx
+++ starters/js-webpack-react/src/js/app.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { hot } from 'react-hot-loader/root';
 import jsImage from '../public/images/js-logo.png';
 
-export const App = () => (
-  <h1>
-    <img src={jsImage} alt="javascript" height="100" />
-    <span>Hello from ES2015+</span>
-  </h1>
-);
+export function App() {
+  return (
+    <h1>
+      <img src={jsImage} alt="javascript" height="100" />
+      <span>Hello from ES2015+</span>
+    </h1>
+  );
+}
 
 export default hot(App);
