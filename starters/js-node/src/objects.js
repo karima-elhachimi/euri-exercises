c7fb8efa51f0d5c795536e4fd4051bae9a56720b
Karima <karima.elhachimi@euri.com>
2019-09-19 08:29:42 +0200

    constructor function

--- starters/js-node/src/objects.js
+++ starters/js-node/src/objects.js
@@ -13,3 +13,22 @@ rabbit.jumps = function(hops) {
 rabbit.eats("grass");
 rabbit.jumps(3);
 
+//constructor
+
+function Animal(){
+    this.eats = function(food){
+        console.log(`I eat ${food}`);
+    }
+}
+
+const cat = new Animal();
+cat.eats("fish");
+cat.meows = function(meows){
+    for(let i = 0; i < meows; i++){
+        console.log("meow");
+    }
+}
+
+cat.meows(4);
+
+
